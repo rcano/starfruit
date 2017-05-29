46e6dd0058ed242b384a45488295e34ae4db86ac
rcano <ioniivil@gmail.com>
2017-05-29 19:56:31 -0300

    fix alert size

--- src/main/scala/starfruit/ui/Utils.scala
+++ src/main/scala/starfruit/ui/Utils.scala
@@ -9,10 +9,12 @@ object Utils {
                foreground: String, background: String, font: String,
                buttons: ButtonType*) = {
     val res = new Alert(Alert.AlertType.INFORMATION, msg, buttons:_*)
+    res.setResizable(true)
     res.getDialogPane.getScene.getStylesheets.addAll(parentScene.getStylesheets)
     val label = res.getDialogPane.lookup(".content").asInstanceOf[Label]
     label.setStyle(s"-fx-text-fill: $foreground; -fx-font: $font")
     label.getParent.setStyle(s"-fx-background-color: $background")
+    res.getDialogPane.getScene.getWindow.sizeToScene()
     res
   }
 }
